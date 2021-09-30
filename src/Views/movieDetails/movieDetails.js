399fb617ac3e364f4f9835420902b7af799bd262
unknown <xandemagnocorreia@gmail.com>
2021-09-30 17:16:52 -0300

    [feat][main] - finalized details page

--- src/Views/movieDetails/movieDetails.js
+++ src/Views/movieDetails/movieDetails.js
@@ -3,7 +3,8 @@ import { useHistory, useParams } from 'react-router';
 import api from '../../services/api';
 import ContainerLoading from '../../components/loading';
 import Header from '../../components/Header/header';
-import { DetailsPage } from './styledMovieDetails';
+import { DetailsPage, DetailsContainer, DetailsPoster, DetailsContent, DetailsTitle, DetailsOverview, DetailsInfo, GenreCard, DetailsInfoContent } from './styledMovieDetails';
+import StarIcon from '@material-ui/icons/Star';
 
 export default function MovieDetails() {
 
@@ -15,7 +16,7 @@ export default function MovieDetails() {
     const getMovieDetails = async () => {
         api.get(`/movie/${id}?api_key=05219aef37ad48f79afaed988d4298e6&language=en-US`)
             .then(res => {
-                setMovieDetails(res.data.results);
+                setMovieDetails(res.data);
                 console.log(res.data);
                 setIsLoading(false);
             })
@@ -31,6 +32,8 @@ export default function MovieDetails() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [id])
 
+    const img_500 = "https://image.tmdb.org/t/p/w500";
+
     return (
 
         <>
@@ -39,7 +42,22 @@ export default function MovieDetails() {
                 {isLoading ?
                     <ContainerLoading />
                     :
-                    <h1>tela de detalhes</h1>
+                    <DetailsContainer>
+                        <DetailsPoster alt={movieDetails.title} src={`${img_500}/${movieDetails.poster_path}`} />
+                        <DetailsContent>
+                            <DetailsTitle>{movieDetails.title}</DetailsTitle>
+                            <DetailsOverview>{movieDetails.overview}</DetailsOverview>
+                            <DetailsInfo>
+                                {movieDetails.genres.map((item, index) => (
+                                    <GenreCard key={index} >{item.name}</GenreCard>
+                                ))}
+                            </DetailsInfo>
+                            <DetailsInfo>
+                                    <DetailsInfoContent style={{ color: "white" }} >{movieDetails.runtime}min</DetailsInfoContent>
+                                    <DetailsInfoContent style={{ color: "#0077be" }} >{movieDetails.vote_average}<StarIcon /></DetailsInfoContent>
+                            </DetailsInfo>
+                        </DetailsContent>
+                    </DetailsContainer>
                 }
             </DetailsPage>
         </>
--- src/Views/movieDetails/styledMovieDetails.js
+++ src/Views/movieDetails/styledMovieDetails.js
@@ -2,8 +2,70 @@ import styled from "styled-components";
 
 export const DetailsPage = styled.div`
     display: flex;
-    width: 100vw;
-    height: 100vh;
+    min-width: 100vw;
+    min-height: 100vh;
     flex-flow: column;
     background-color: #0d253f;
+`;
+
+export const DetailsContainer = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    /* background-color: red; */
+    justify-content: space-evenly;
+    @media(max-width: 1024px){
+        flex-flow: column;
+        align-items: center;
+    }
+`;
+
+export const DetailsPoster = styled.img`
+    border-radius: 15px;
+    max-width: 90%;
+`;
+
+export const DetailsContent = styled.div`
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    /* background-color: blue; */
+`;
+
+export const DetailsTitle = styled.h1`
+    color: white;
+`;
+
+export const DetailsOverview = styled.p`
+    background-color: #282c34;
+    color: white;
+    border-radius: 15px;
+    font-size: 1rem;
+    padding: 10px;
+    min-height: 200px;
+    max-width: 500px;
+    min-width: 200px;
+`;
+
+export const DetailsInfo = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+`;
+
+export const DetailsInfoContent = styled.p`
+    margin-block-start: 1;
+    margin-block-end: 1;
+    font-size: 1.3rem;
+    margin-left: 10px;
+    font-weight: lighter;
+    padding-bottom: 5px;
+`;
+
+export const GenreCard = styled.div`
+    border-radius: 20px;
+    margin: 2px;
+    padding: 5px;
+    font-weight: bold;
+    background-color: #f4f4f4;
+    color: orange;
 `;
